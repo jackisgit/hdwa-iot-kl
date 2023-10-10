71dfac8cbd7a7afcdd7b45651f517becd4461567
chenxin <zhr19960307.zhr>
2023-10-10 09:56:57 +0800

    齐齐哈尔

--- startup.bat
+++ startup.bat
@@ -1,3 +1,3 @@
 title IEPC_KL
 chcp 65001
-for %%j in (iot*.jar) do  java -jar -Dfile.encoding=utf-8 -Dspring.profiles.active=qiqihaer  %%j
+for %%j in (iot*.jar) do  java -javaagent:%%j -jar -Dfile.encoding=utf-8 -Dspring.profiles.active=qiqihaer  %%j
